111958fd6a0315e5fb7ac604b465f2ede263e5bf
Jeff Meunier <jeffrey.a.meunier@gmail.com>
2022-08-19 06:57:09 -0400

    Changed return value of file:readAll.

--- src/data/file.c
+++ src/data/file.c
@@ -90,12 +90,9 @@ char file_readChar(struct D_File* self) {
 // This returns the number of bytes read, but since the number is an int
 // and the number of bytes read can be an unsigned long, the integer is
 // split into two parts returned as an array.
-struct D_Array* file_readAll(struct D_File* self, struct D_StringBuffer* stringBuffer, struct Evaluator* etor) {
+size_t file_readAll(struct D_File* self, struct D_StringBuffer* stringBuffer, struct Evaluator* etor) {
     size_t nBytesRead = file_readAll_stringBuffer(self, stringBuffer, etor);
-    int n1 = (int)(nBytesRead / INT_MAX);
-    int n2 = (int)(nBytesRead % INT_MAX);
-    struct D_Array* sizeAry = array_newN(2, integer_new(n1), integer_new(n2));
-    return sizeAry;
+    return nBytesRead;
 }
 
 size_t file_readAll_stringBuffer(struct D_File* self, struct D_StringBuffer* stringBuffer, struct Evaluator* etor) {
