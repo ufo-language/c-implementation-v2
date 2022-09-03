4590ec63b25d9f9c5d875d71b07779f6cf7f0538
Jeff Meunier <jeffrey.a.meunier@gmail.com>
2022-09-03 06:33:38 -0400

    defines.h: Added PRODUCTION/RELEASE defines.

--- src/defines.h
+++ src/defines.h
@@ -3,6 +3,10 @@
 
 #define OS_PLATFORM "Linux"
 //#define OS_PLATFORM "MacOS"
-//#define OS_PLATFORM "Windows"
+//#define OS_PLATFORM "Windows"]
+//#define OS_PLATFORM "Minix"
+
+#define PRODUCTION 1
+#define RELEASE 0
 
 #endif
