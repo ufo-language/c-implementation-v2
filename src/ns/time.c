d83281fce14b14af8de7eeae53c498d9260762ec
Jeff Meunier <jeffrey.a.meunier@gmail.com>
2022-09-04 07:10:36 -0400

    time.c: Added time:cpuTime() primitive.

--- src/ns/time.c
+++ src/ns/time.c
@@ -6,6 +6,7 @@
 #include "data/integer.h"
 #include "data/list.h"
 #include "data/primitive.h"
+#include "data/real.h"
 #include "data/string.h"
 #include "data/symbol.h"
 #include "etor/evaluator.h"
@@ -14,40 +15,39 @@
 
 #define NS_NAME "time"
 
+static void _cpuTime(struct Evaluator* etor, struct D_List* args);
 static void _now(struct Evaluator* etor, struct D_List* args);
 
 void ns_time_defineAll(struct D_HashTable* env) {
     struct E_Identifier* nsName = identifier_new(NS_NAME);
     struct D_HashTable* nsHash = hashTable_new();
     hashTable_put_unsafe(env, (struct Any*)nsName, (struct Any*)nsHash);
+    primitive_define(nsHash, "cpuTime", _cpuTime);
     primitive_define(nsHash, "now", _now);
 }
 
 extern char** tzname;
 void tzset(void);
 
+static void _cpuTime(struct Evaluator* etor, struct D_List* args) {
+    primitive_checkArgs(0, NULL, args, NULL, etor);
+    struct D_Real* timeReal = real_new((double)clock() / (double)CLOCKS_PER_SEC);
+    evaluator_pushObj(etor, (struct Any*)timeReal);
+}
+
 static void _now(struct Evaluator* etor, struct D_List* args) {
     primitive_checkArgs(0, NULL, args, NULL, etor);
     time_t t = time(NULL);
     struct tm *tmStruct = localtime(&t);
-    int sec = tmStruct->tm_sec;
-    int min = tmStruct->tm_min;
-    int hour = tmStruct->tm_hour;
-    int mday = tmStruct->tm_mday;
-    int mon = tmStruct->tm_mon;
-    int year = tmStruct->tm_year;
-    int wday = tmStruct->tm_wday;
-    int yday = tmStruct->tm_yday;
-    int isdst = tmStruct->tm_isdst;
     struct D_HashTable* timeHash = hashTable_new();
-    hashTable_put_unsafe(timeHash, (struct Any*)symbol_new("Second"), (struct Any*)integer_new(sec));
-    hashTable_put_unsafe(timeHash, (struct Any*)symbol_new("Minute"), (struct Any*)integer_new(min));
-    hashTable_put_unsafe(timeHash, (struct Any*)symbol_new("Hour"), (struct Any*)integer_new(hour));
-    hashTable_put_unsafe(timeHash, (struct Any*)symbol_new("DayOfMonth"), (struct Any*)integer_new(mday));
-    hashTable_put_unsafe(timeHash, (struct Any*)symbol_new("Month"), (struct Any*)integer_new(mon));
-    hashTable_put_unsafe(timeHash, (struct Any*)symbol_new("Year"), (struct Any*)integer_new(year+1900));
-    hashTable_put_unsafe(timeHash, (struct Any*)symbol_new("Weekday"), (struct Any*)integer_new(wday));
-    hashTable_put_unsafe(timeHash, (struct Any*)symbol_new("DayOfYear"), (struct Any*)integer_new(yday));
-    hashTable_put_unsafe(timeHash, (struct Any*)symbol_new("IsDST"), (struct Any*)boolean_from(isdst));
+    hashTable_putSymInt(timeHash, "Second", tmStruct->tm_sec);
+    hashTable_putSymInt(timeHash, "Minute", tmStruct->tm_min);
+    hashTable_putSymInt(timeHash, "Hour", tmStruct->tm_hour);
+    hashTable_putSymInt(timeHash, "DayOfMonth", tmStruct->tm_mday);
+    hashTable_putSymInt(timeHash, "Month", tmStruct->tm_mon);
+    hashTable_putSymInt(timeHash, "Year", tmStruct->tm_year + 1900);
+    hashTable_putSymInt(timeHash, "Weekday", tmStruct->tm_wday);
+    hashTable_putSymInt(timeHash, "DayOfYear", tmStruct->tm_yday);
+    hashTable_put_unsafe(timeHash, (struct Any*)symbol_new("IsDST"), (struct Any*)boolean_from(tmStruct->tm_isdst));
     evaluator_pushObj(etor, (struct Any*)timeHash);
 }
