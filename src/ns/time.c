d14c2c72bb584348e73de43bc55a85234ee6a6bf
Jeff Meunier <jeffrey.a.meunier@gmail.com>
2022-09-07 21:01:43 -0400

    time.c: Added time:elapsed() primitive,

--- src/ns/time.c
+++ src/ns/time.c
@@ -18,6 +18,7 @@
 
 static void _cpu(struct Evaluator* etor, struct D_List* args);
 static void _current(struct Evaluator* etor, struct D_List* args);
+static void _elapsed(struct Evaluator* etor, struct D_List* args);
 static void _now(struct Evaluator* etor, struct D_List* args);
 static void _string(struct Evaluator* etor, struct D_List* args);
 
@@ -27,6 +28,7 @@ void ns_time_defineAll(struct D_HashTable* env) {
     hashTable_put_unsafe(env, (struct Any*)nsName, (struct Any*)nsHash);
     primitive_define(nsHash, "cpu", _cpu);
     primitive_define(nsHash, "current", _current);
+    primitive_define(nsHash, "elapsed", _elapsed);
     primitive_define(nsHash, "now", _now);
     primitive_define(nsHash, "string", _string);
 }
@@ -54,11 +56,42 @@ static void _current(struct Evaluator* etor, struct D_List* args) {
     evaluator_pushObj(etor, (struct Any*)timeHash);
 }
 
+static void _elapsed(struct Evaluator* etor, struct D_List* args) {
+    static enum TypeId paramTypes[] = {T_List};
+    struct Any* listObj;
+    struct Any** paramVars[] = {&listObj};
+    primitive_checkArgs(1, paramTypes, args, paramVars, etor);
+    struct D_List* pair = (struct D_List*)listObj;
+    static enum TypeId paramTypes2[] = {T_Integer, T_Integer};
+    struct Any* secObj;
+    struct Any* usecObj;
+    struct Any** paramVars2[] = {&secObj, &usecObj};
+    primitive_checkArgs(2, paramTypes2, pair, paramVars2, etor);
+    struct D_Integer* secInt = (struct D_Integer*)secObj;
+    struct D_Integer* usecInt = (struct D_Integer*)usecObj;
+    int sec1 = integer_getValue(secInt);
+    int usec1 = integer_getValue(usecInt);
+    struct timeval te;
+    gettimeofday(&te, NULL);
+    int secElapsed = te.tv_sec - sec1;
+    int usecElapsed = te.tv_usec - usec1;
+    if (usecElapsed < 0) {
+        secElapsed--;
+        usecElapsed += 1e6;
+    }
+    struct D_List* elapsedPair = list_from((struct Any*)integer_new(secElapsed),
+                                        (struct Any*)integer_new(usecElapsed),
+                                        NULL);
+    evaluator_pushObj(etor, (struct Any*)elapsedPair);
+}
+
 static void _now(struct Evaluator* etor, struct D_List* args) {
     primitive_checkArgs(0, NULL, args, NULL, etor);
-    struct timeval te; 
+    struct timeval te;
     gettimeofday(&te, NULL);
-    struct D_List* pair = list_new((struct Any*)integer_new(te.tv_sec), (struct Any*)integer_new(te.tv_usec));
+    struct D_List* pair = list_from((struct Any*)integer_new(te.tv_sec),
+                                    (struct Any*)integer_new(te.tv_usec),
+                                    NULL);
     evaluator_pushObj(etor, (struct Any*)pair);
 }
 
