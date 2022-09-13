aa306a1cdd33d3240c99ea44f1d06a03b0faf53e
Jeff Meunier <jeffrey.a.meunier@gmail.com>
2022-09-13 08:53:22 -0400

    Removed concurrent resource server process.

--- src/ipc/resourceserver.h
+++
@@ -1,8 +0,0 @@
-#if !defined(RESOURCESERVER_H)
-#define RESOURCESERVER_H
-
-#include <sys/types.h>
-
-void resourceServer(pid_t childPid, int readFromUFO, int writeToUFO);
-
-#endif
--- src/main.c
+++ src/main.c
@@ -1,76 +1,32 @@
 #include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <time.h>
-#include <unistd.h>
-
-#if defined(linux)
-#include <sys/prctl.h>
-#endif
 
 #include "gc/gc.h"
-#include "ipc/ipc.h"
-#include "ipc/resourceserver.h"
 #include "main/ufo.h"
 
 int ARGC = 0;
 char** ARGV = NULL;
-
-extern int PARENT_TO_CHILD[2];
-extern int CHILD_TO_PARENT[2];
 
 void any_sanityCheck(void);
 void methodTable_setupMethods(void);
 void methodTable_deallocateAll(void);
 
 int main(int argc, char* argv[]) {
+#if defined(linux)
+    char* name = "ufo_evaluator";
+    if (prctl(PR_SET_NAME, name, NULL, NULL, NULL) < 0) {
+        perror("prctl()");
+    }
+#endif
     ARGC = argc;
     ARGV = argv;
-    if (pipe(PARENT_TO_CHILD) == -1 || pipe(CHILD_TO_PARENT) == -1) {
-        fprintf(stderr, "Unable to open parent/child pipes\n");
-        exit(1);
-    }
     srand(time(0));
-    //pid_t parentPid = getpid();
-    pid_t pid = fork();
-    if (pid == 0) {
-        // child = UFO evaluator
-        #if defined(linux)
-        char* name = "ufo_evaluator";
-        if (prctl(PR_SET_NAME, name, NULL, NULL, NULL) < 0) {
-            perror("prctl()");
-        }
-        #endif
-        //printf("I am the child\n");
-        //char string[32] = {0};
-        //ipc_readString(PARENT_TO_CHILD[0], 5, string);
-        //printf("child read string '%s'\n", string);
-        //string[0] = 'h';
-        //ipc_writeString(CHILD_TO_PARENT[1], 5, string);
-        any_sanityCheck();
-        methodTable_setupMethods();
-        gc_start();
-        ufo_start();  // this starts the REPL
-        ufo_stop();
-        gc_stop();
-        methodTable_deallocateAll();
-        ipc_writeString(CHILD_TO_PARENT[1], 16, "DShutting down\n\0");
-        ipc_writeString(CHILD_TO_PARENT[1], 2, "UQ");
-    }
-    else {
-        // parent = resource server
-        #if defined(linux)
-        char* name = "ufo_server";
-        if (prctl(PR_SET_NAME, name, NULL, NULL, NULL) < 0) {
-            perror("prctl()");
-        }
-        #endif
-        //printf("I am the parent\n");
-        //ipc_writeString(PARENT_TO_CHILD[1], 5, "Hello");
-        //char string[32] = {0};
-        //ipc_readString(CHILD_TO_PARENT[0], 5, string);
-        //printf("parent got sting '%s'\n", string);
-        resourceServer(pid, CHILD_TO_PARENT[0], PARENT_TO_CHILD[1]);
-    }
+    any_sanityCheck();
+    methodTable_setupMethods();
+    gc_start();
+    ufo_start();  // this starts the REPL
+    ufo_stop();
+    gc_stop();
+    methodTable_deallocateAll();
+    return 0;
 }
